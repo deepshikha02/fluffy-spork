e7a5b8783c80fe539653265b3a2f2e1a72ffbd30
Deepshikha Mohanta <deepshikhamohanta@Administrators-MacBook-Pro.local>
2021-12-03 14:44:11 +0530

    defaulting social media links until  further  decided

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -72,7 +72,7 @@ export default function HeaderLinks(props) {
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
-            href="https://twitter.com/CreativeTim?ref=creativetim"
+            href="https://twitter.com"
             target="_blank"
             color="transparent"
             className={classes.navLink}
@@ -90,7 +90,7 @@ export default function HeaderLinks(props) {
         >
           <Button
             color="transparent"
-            href="https://www.facebook.com/CreativeTim?ref=creativetim"
+            href="https://www.facebook.com"
             target="_blank"
             className={classes.navLink}
           >
@@ -107,7 +107,7 @@ export default function HeaderLinks(props) {
         >
           <Button
             color="transparent"
-            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+            href="https://www.instagram.com"
             target="_blank"
             className={classes.navLink}
           >
